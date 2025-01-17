d38f771a1195b77011884b2f6231ccc29e879d4c
Aaron Kurian Abraham <147163014+AaronKurian@users.noreply.github.com>
2025-01-17 20:38:36 +0000

    Seventh Commit

--- app.py
+++ app.py
@@ -89,5 +89,4 @@ def send():
 if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080, debug=True)
